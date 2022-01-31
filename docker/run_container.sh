52c89db66e71a3c6f2748d50b2b8e9cc7ff42c36
Garoe <garoe.dorta@protonmail.com>
2022-01-31 20:53:19 +0100

    Set bind point for develop to root

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add this line to mount points for easier development
-# -v $(dirname ${CURRENT_DIR})/src:/home/gradle/signalblast/src \
+# -v $(dirname ${CURRENT_DIR})/src:/root/signalblast/src \
 
 CURRENT_DIR=$(dirname $(realpath $0))
 docker container run \
