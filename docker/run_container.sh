290cac9d2b3c27dc48bc78e5c27504616c55b125
Garoe <garoe.dorta@protonmail.com>
2022-01-31 20:39:24 +0100

    Bind src in docker for easy develop

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker container run -v $(pwd)/run:/signald --interactive=true --tty=true signalblast:latest /bin/bash
+CURRENT_DIR=$(dirname $(realpath $0))
+docker container run \
+  -v ${CURRENT_DIR}/run:/signald \
+  \ # -v $(dirname ${CURRENT_DIR})/src:/home/gradle/signalblast/src \ # Uncomment this line for develop
+  --interactive=true \
+  --tty=true \
+  signalblast:latest /bin/bash
