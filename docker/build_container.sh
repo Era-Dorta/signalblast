7642dea6cf2d2bdd50350e10838a3f0424f29617
Era Dorta <era.xama@protonmail.com>
2022-02-15 23:32:36 +0100

    Check num arguments in build script

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname $(realpath $0))
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Missing commit_id argument"
+    exit 1
+fi
+
 docker build --build-arg COMMIT_ID=$1 --tag signalblast ${CURRENT_DIR}
