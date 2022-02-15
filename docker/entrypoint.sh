5ccd79af3462a6b8ef121526efad56a253508b32
Era Dorta <era.xama@protonmail.com>
2022-02-16 00:21:48 +0100

    Waiting for a bit longer in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,17 +12,17 @@ sleep 5s
 # Wait for the user to link their phone with signald
 while [[ "$(signaldctl account list --output-format yaml)" == "[]" ]]; do
     sleep 3
-    echo "Waiting for account registration" > $SIGNALBLAST_LOG_FILE
+    echo "Waiting for account registration" >> $SIGNALBLAST_LOG_FILE
 done
 
-echo "An account was registered" > $SIGNALBLAST_LOG_FILE
+echo "An account was registered" >> $SIGNALBLAST_LOG_FILE
 
 source /root/signalblast/docker/phone_number.sh
 
 # Signald needs again some time after phone linking
-echo "Waiting for signald to get ready" > $SIGNALBLAST_LOG_FILE
-sleep 5s
-echo "Starting the signalblast bot" > $SIGNALBLAST_LOG_FILE
+echo "Waiting for signald to get ready" >> $SIGNALBLAST_LOG_FILE
+sleep 15s
+echo "Starting the signalblast bot" >> $SIGNALBLAST_LOG_FILE
 
 python3 /root/signalblast/signalblast/broadcastbot.py
 
