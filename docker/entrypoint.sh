02490e0a5a6ca9e574fdfae89a86833913944bf3
Era Dorta <era.xama@protonmail.com>
2022-02-15 23:38:07 +0100

    More fixes in entryscript

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,11 +4,14 @@ export SIGNALD_TRUST_NEW_KEYS=true
 
 signald &> /var/log/signald.log &
 
-while [ $(signaldctl account list --output-format yaml) = "[]" ]; do
+while [[ "$(signaldctl account list --output-format yaml)" == "[]" ]]; do
     sleep 3
     echo "waiting for account setup"
 done
 
 source /root/signalblast/phone_number.sh
+
+echo $SIGNAL_PHONE_NUMBER
+
 python3 /root/signalblast/signalblast/broadcastbot.py;
 
