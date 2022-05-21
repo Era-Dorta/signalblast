92f3aaab3b13ce2008ef6e92687a381607650de3
Era Dorta <era.xama@protonmail.com>
2022-05-21 17:41:22 +0200

    Formatting

--- docker/run_signald.sh
+++ docker/run_signald.sh
@@ -2,8 +2,8 @@
 
 SIGNALD_BASE="${SIGNALD_BASE:-$(pwd)}"
 
-docker run -v \
-"${SIGNALD_BASE}/data/signald:/signald" \
+docker run \
+-v "${SIGNALD_BASE}/data/signald:/signald" \
 -e SIGNALD_TRUST_NEW_KEYS=true \
 -e SIGNALD_TRUST_ALL_KEYS=true \
 signald/signald:0.18.5-non-root
