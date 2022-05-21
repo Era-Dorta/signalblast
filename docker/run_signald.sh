3b4c1e0ab088c0fca9296a2be855e64af0903c2b
Era Dorta <era.xama@protonmail.com>
2022-05-21 17:31:06 +0200

    Using non-root version of signald and trusting new keys

--- docker/run_signald.sh
+++ docker/run_signald.sh
@@ -2,4 +2,8 @@
 
 SIGNALD_BASE="${SIGNALD_BASE:-$(pwd)}"
 
-docker run -v "${SIGNALD_BASE}/data/signald:/signald" signald/signald:0.18.0
+docker run -v \
+"${SIGNALD_BASE}/data/signald:/signald" \
+-e SIGNALD_TRUST_NEW_KEYS=true \
+-e SIGNALD_TRUST_ALL_KEYS=true \
+signald/signald:0.18.5-non-root
