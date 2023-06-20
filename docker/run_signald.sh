7423ac56e5267649d237170b49c6307508482677
Era Dorta <era.xama@protonmail.com>
2023-06-20 22:11:30 +0200

    Update signald to version 0.23.2

--- docker/run_signald.sh
+++ docker/run_signald.sh
@@ -6,4 +6,4 @@ docker run \
 -v "${SIGNALD_BASE}/data/signald:/signald" \
 -e SIGNALD_TRUST_NEW_KEYS=true \
 -e SIGNALD_TRUST_ALL_KEYS=true \
-signald/signald:0.18.5-non-root
+registry.gitlab.com/signald/signald:0.23.2
