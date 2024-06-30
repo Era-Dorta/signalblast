e3df51dc97014f74d9e99005b899fee5ed02dc78
Era Dorta <era.dorta@proton.me>
2024-06-30 21:37:54 +0200

    Add restart flag to signal-cli-rest-api

--- docker/run_signal_cli_rest_api.sh
+++ docker/run_signal_cli_rest_api.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run --name signal-api-json-rpc -p 8080:8080 \
+    --restart=unless-stopped \
     -v $HOME/.local/share/signal-api:/home/.local/share/signal-cli \
     -e 'MODE=json-rpc' \
     -e 'AUTO_RECEIVE_SCHEDULE_SEND_READ_RECEIPTS=true' \
