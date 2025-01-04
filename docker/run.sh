54976a0780e018f868dde6ad0214e94abfafcf3a
Era Dorta <era.dorta@proton.me>
2025-01-04 20:54:46 +0100

    Update docker run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Add these for easier development
+# REPO_DIR=$(dirname $(dirname $(realpath $0)))
+#
 #  -v $(dirname ${REPO_DIR}):/home/user/signalblast/ \
 #  --interactive=true \
 #  --tty=true \
 #  --entrypoint bash \
 
-SIGNALBLAST_BASE="${SIGNALBLAST_BASE:-$(pwd)}"
+SIGNALBLAST_VERSION=$(uvx hatch version)
+DOCKER_TAG="${SIGNALBLAST_VERSION//+/-}"
 
-REPO_DIR=$(dirname $(dirname $(realpath $0)))
-docker container run \
-  --rm \
+docker run \
+ --rm \
  -v $HOME/.local/share/signal-api/:/home/user/.local/share/signal-api/ \
  --interactive=true \
  --tty=true \
  --entrypoint bash \
  --network host \
-  eradorta/signalblast:0.5.0
+  eradorta/signalblast:$DOCKER_TAG
