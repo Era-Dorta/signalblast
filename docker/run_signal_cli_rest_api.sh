7b14d4c5e1a15175ee7a4db93a26566cfe00eaf3
Era Dorta <era.dorta@proton.me>
2025-05-29 15:48:01 +0200

    fix: removed env variable for signal-cli-rest-api
    The variable does nothing as it is not used in the json-rpc mode.

--- docker/run_signal_cli_rest_api.sh
+++ docker/run_signal_cli_rest_api.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker run --name signal-api-json-rpc -p 8080:8080 \
-    --restart=always \
+docker run --rm -p 8080:8080 \
     -v $HOME/.local/share/signal-api:/home/.local/share/signal-cli \
-    -e 'MODE=json-rpc' \
-    -e 'AUTO_RECEIVE_SCHEDULE_SEND_READ_RECEIPTS=true' \
+    -e MODE='json-rpc' \
     bbernhard/signal-cli-rest-api:0.92
