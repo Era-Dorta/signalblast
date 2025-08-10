385bf3f5e8d3edbe2961a2ea9e81d984d36d9fed
Era Dorta <era.dorta@proton.me>
2025-08-10 19:06:46 +0200

    build: update signal-cli-rest-api to version 0.94

--- docker/run_signal_cli_rest_api.sh
+++ docker/run_signal_cli_rest_api.sh
@@ -3,4 +3,4 @@
 docker run --rm -p 8080:8080 \
     -v $HOME/.local/share/signal-api:/home/.local/share/signal-cli \
     -e MODE='json-rpc' \
-    bbernhard/signal-cli-rest-api:0.92
+    bbernhard/signal-cli-rest-api:0.94
